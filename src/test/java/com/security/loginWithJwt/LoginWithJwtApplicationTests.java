350cb32e5602c4ccbd00547aca413b899ff76bf0
a38930 <quannguyen16902hn@gmail.com>
2024-08-01 01:31:58 +0700

    first

---
+++ src/test/java/com/security/loginWithJwt/LoginWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.loginWithJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
